5a615be0386caf11c3952e324f210f80696fe1e3
Jeison Cardona Gómez <jeison.cardona@correounivalle.edu.co>
2018-04-21 20:07:53 -0500

    + Corrección de iteraciones.

--- src/main.cpp
+++ src/main.cpp
@@ -34,11 +34,10 @@ int main( int argc, const char* argv[] ){
     }else if( option == "3" ){ 
             double matrix[3][3] = {{6,5,-5},{2,6,-2},{2,5,-1}};
             double *x= new double[3];
-            int iteraciones = 200;
+            int iteraciones = 25;
             x[0]=3;
             x[1]=7;
             x[2]=-13;
-            iteraciones = 25;   
             double matrix_L[3][3] = {{1, 0, 0},{(1/(double)3), 1, 0},{(1/(double)3), (10/(double)13), 1}}; 
             double matrix_U[3][3] = {{6, 5, -5},{0, (13/(double)3), (-1/(double)3)},{0, 0, (12/(double)13)}}; 
             cout << "\n     último r: " << metodos.inverse_power_method( matrix_L, matrix_U, x, iteraciones ) << "\n" << endl;
