b2207a4778187e193d6fdf03fcb9675bb72b679b
Jeison Cardona Gómez <jeison.cardona@correounivalle.edu.co>
2018-04-21 20:17:14 -0500

    + Corrección a 200 iteraciones.

--- src/main.cpp
+++ src/main.cpp
@@ -13,7 +13,7 @@ int main( int argc, const char* argv[] ){
      
     if( option == "1" ){
 
-            int n = 3; double *y = new double[n]; double *x= new double[n]; int iteraciones = 100;
+            int n = 3; double *y = new double[n]; double *x= new double[n]; int iteraciones = 200;
             int matrix[3][3] = {{6,5,-5},{2,6,-2},{2,5,-1}}; 
             x[0]=1;
             x[1]=2;
